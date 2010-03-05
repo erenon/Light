25f0642d8d23286f1cf7fa73f0d761730e523383
erenon <erenon2@gmail.com>
2010-03-05 23:06:50 +0100

    Light library initial commit. Light_Exception and Light_Exception_NotFound added, not yet tested.

---
+++ library/Light/Exception.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * General exception
+ *
+ * @category   Light
+ * @package    Light_Library
+ * @subpackage Exception
+ * @license    New BSD License
+ * @version    $Id:$
+ * @author erenon
+ */
+
+class Light_Exception extends Exception
+{
+}
---
+++ library/Light/Exception/NotFound.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Not found exception
+ *
+ * @category   Light
+ * @package    Light_Library
+ * @subpackage Exception
+ * @license    New BSD License
+ * @version    $Id:$
+ * @author erenon
+ */
+
+require_once 'Light/Exception.php';
+
+class Light_Exception_NotFound extends Light_Exception
+{
+}
