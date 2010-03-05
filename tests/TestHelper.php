39834467ecc1ca5697bb8a1da92a0c1c27af2e36
erenon <erenon2@gmail.com>
2010-03-05 23:07:37 +0100

    TestHelper initial commit, adds Light library to the top of the include path.

---
+++ tests/TestHelper.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Setup test environment
+ *
+ * @category   Light
+ * @package    Light_Test
+ * @license    New BSD License
+ * @version    $Id:$
+ * @author erenon
+ */
+
+/**
+ * Add application library to the include path.
+ * This hardcoded path may change if application config is presented
+ */
+
+$lightRoot   = realpath(dirname(dirname(__FILE__)));
+$libraryPath = $lightRoot . "/library/";
+
+$includePath = array(
+    $libraryPath,
+    get_include_path()
+    );
+set_include_path(implode(PATH_SEPARATOR, $includePath));
