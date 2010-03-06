65f91ae7d06931c6e58afe12ea5a2aea90840e2c
erenon <erenon2@gmail.com>
2010-03-06 17:44:15 +0100

    InvalidParameters exception added to the Light library

---
+++ library/Light/Exception/InvalidParameters.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Invalid parameters exception
+ *
+ * Usecases:
+ * - Wrong parameters given
+ *
+ * @category   Light
+ * @package    Light_Exception
+ * @license    New BSD License
+ * @version    $Id:$
+ * @author erenon
+ */
+
+require_once 'Light/Exception.php';
+
+class Light_Exception_InvalidParameters extends Light_Exception
+{
+}
