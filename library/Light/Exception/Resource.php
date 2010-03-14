a37f5c492b14be243bf23bcff944da7abed8a556
erenon <erenon2@gmail.com>
2010-03-14 20:48:06 +0100

    Light_Exception_Resource added

---
+++ library/Light/Exception/Resource.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Resource exception
+ *
+ * Usecases: file/dir not writable, database, socket not accessible
+ *
+ * @category   Light
+ * @package    Light_Library
+ * @subpackage Exception
+ * @license    New BSD License
+ * @version    $Id:$
+ * @author erenon
+ */
+
+require_once 'Light/Exception.php';
+
+class Light_Exception_Resource extends Light_Exception
+{
+}
