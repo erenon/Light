5eed09ac0a95d5f97f01a233f97ab38f9519ee35
erenon <erenon2@gmail.com>
2010-03-14 23:15:58 +0100

    Page form skeleton added

---
+++ application/modules/default/forms/Page.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Simple form to create and edit Pages
+ *
+ * @category   Light
+ * @package    Light_Page
+ * @subpackage Form
+ * @license    New BSD License
+ * @version    $Id:$
+ * @author erenon
+ */
+
+require_once 'Zend/Form.php';
+
+class Default_Form_Page extends Zend_Form
+{
+
+}
