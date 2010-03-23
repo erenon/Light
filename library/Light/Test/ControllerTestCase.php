86174ee114c332a27e93c3f5e0056a20c796e324
erenon <erenon2@gmail.com>
2010-03-23 17:24:32 +0100

    Light_Test_* added as common method wrapper

---
+++ library/Light/Test/ControllerTestCase.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Wrapper class to implement some common methods
+ *
+ * @category   Light
+ * @package    Light_Library
+ * @subpackage Test
+ * @license    New BSD License
+ * @version    $Id:$
+ * @author erenon
+ */
+
+require_once 'Zend/Application.php';
+
+/**
+ * Wrapper class to implement some common methods
+ *
+ * @category   Light
+ * @package    Light_Library
+ * @subpackage Test
+ * @license    New BSD License
+ * @version    $Id:$
+ * @author erenon
+ */
+abstract class Light_Test_ControllerTestCase
+    extends Zend_Test_PHPUnit_ControllerTestCase
+{
+    public function setUp()
+    {
+        $this->application = new Zend_Application(
+            'testing',
+            APPLICATION_PATH . '/configs/config.php'
+        );
+
+        $this->bootstrap = array(
+            $this->application,
+            'bootstrap'
+        );
+
+        parent::setUp();
+    }
+}
