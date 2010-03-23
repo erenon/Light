480aec5575857023870997a9fbc4da6a69cfb99f
erenon <erenon2@gmail.com>
2010-03-23 17:23:16 +0100

    Config files added, only for testing purposes

---
+++ application/configs/config.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Main config
+ *
+ * @category   Light
+ * @package    Light_Application
+ * @subpackage Config
+ * @license    New BSD License
+ * @version    $Id:$
+ * @author erenon
+ */
+
+return array_merge_recursive(array(
+    'resources'   => array(
+        'frontController' => array(
+            'moduleDirectory' => APPLICATION_PATH . '/modules',
+            'moduleControllerDirectoryName' => '/controllers',
+            'defaultModule' => 'default',
+            'defaultAction' => 'index',
+            'defaultControllerName' => 'index',
+
+            'throwerrors' => false,
+            'env' => APPLICATION_ENV
+        ),
+    )
+), include dirname(__FILE__) . '/' . APPLICATION_ENV . '.config.php');
---
+++ application/configs/testing.config.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Testing subconfig
+ *
+ * @category   Light
+ * @package    Light_Application
+ * @subpackage Config
+ * @license    New BSD License
+ * @version    $Id:$
+ * @author erenon
+ */
+
+return array(
+    'resources'      => array(
+        'frontController' => array(
+            'noErrorHandler'  => true,
+            'throwExceptions' => true
+        ),
+    )
+);
