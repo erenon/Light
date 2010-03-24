b7347eb66bf0f2ea6ef47e11d625092ca0d3205d
erenon <erenon2@gmail.com>
2010-03-24 20:33:58 +0100

    Incomplete integration test chunk of default page controller

---
+++ tests/integration/PageTest.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Default_PageController test
+ *
+ * @category   Light
+ * @package    Light_Test
+ * @subpackage Integration
+ * @license    New BSD License
+ * @version    $Id:$
+ * @author erenon
+ */
+
+require_once 'Light/Service/Abstract.php';
+
+require_once 'Light/Test/ControllerTestCase.php';
+
+/**
+ * Default_PageController test
+ *
+ * @category Light
+ * @package Light_Test
+ * @subpackage Integration
+ * @license New BSD License
+ * @author erenon
+ *
+ * @group Light_Page
+ * @group Light_Integration
+ *
+ * @todo remove service expectations
+ * @todo check for output
+ *
+ */
+class Integration_PageTest extends Light_Test_ControllerTestCase
+{
+    public function testShowCallsServiceFind()
+    {
+        $content = 'foo';
+        $language = 'bar';
+
+        $service = $this->getMock('Default_Page_Service', array('find'));
+        $service->expects($this->once())
+                ->method('find')
+                ->with($content, $language);
+
+        Light_Service_Abstract::setService($service, 'Page', 'Default');
+
+        $this->request->setQuery(array(
+            'content' => $content,
+            'language' => $language,
+        ));
+
+        $this->dispatch('/page/show');
+
+        $this->assertModule('default');
+        $this->assertController('page');
+        $this->assertAction('show');
+    }
+}
