4450fcf9df89313676ffa5edc90b36414029a77d
erenon <erenon2@gmail.com>
2010-03-24 20:32:16 +0100

    Standard unittest of page controller added

--- tests/application/modules/default/controllers/PageTest.php
+++ tests/application/modules/default/controllers/PageTest.php
@@ -10,9 +10,13 @@
  * @author erenon
  */
 
+require_once '../application/modules/default/controllers/PageController.php';
+
 require_once 'Light/Service/Abstract.php';
 
-require_once 'Light/Test/ControllerTestCase.php';
+require_once 'Zend/Controller/Request/Abstract.php';
+require_once 'Zend/Controller/Response/Abstract.php';
+require_once 'Zend/View.php';
 
 /**
  * Default_PageController test
@@ -27,30 +31,56 @@ require_once 'Light/Test/ControllerTestCase.php';
  *
  */
 class Application_Modules_Default_Controllers_PageTest
-    extends Light_Test_ControllerTestCase
+    extends PHPUnit_Framework_TestCase
 {
     public function testShowCallsServiceFind()
     {
         $content = 'foo';
         $language = 'bar';
 
+        $page = $this->getMock('Default_Model_Page');
+
         $service = $this->getMock('Default_Page_Service', array('find'));
         $service->expects($this->once())
                 ->method('find')
-                ->with($content, $language);
+                ->with($content, $language)
+                ->will($this->returnValue($page));
 
         Light_Service_Abstract::setService($service, 'Page', 'Default');
 
-        $this->request->setQuery(array(
-            'content' => $content,
-            'language' => $language,
-        ));
+        $request = $this->getMock('Zend_Controller_Request_Abstract', array('getParam'));
+        $request->expects($this->any())
+                ->method('getParam')
+                ->will($this->returnCallback(array($this, 'getParam')));
 
-        $this->dispatch('/page/show');
+        $response = $this->getMock('Zend_Controller_Response_Abstract');
 
-        $this->assertModule('default');
-        $this->assertController('page');
-        $this->assertAction('show');
+        $controller = new PageController($request, $response);
 
+        //$view = $this->getMock('Zend_View');
+        $view = new Zend_View();
+        $controller->view = $view;
+
+        $controller->showAction();
+
+        $this->assertEquals(
+            $page,
+            $view->page
+        );
+
+    }
+
+    public function getParam($key)
+    {
+        $parameters = array(
+            'content'  => 'foo',
+            'language' => 'bar',
+        );
+
+        if ( isset($parameters[$key])) {
+            return $parameters[$key];
+        } else {
+            return null;
+        }
     }
 }
